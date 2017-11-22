b5d19cc1459a083612a7313f22b9370b7fdb6d6d
prozax <72GLg&o$Rv#7nHcw>
2017-11-22 23:48:16 +0100

    added command line parameters

--- main.cpp
+++ main.cpp
@@ -1,32 +1,61 @@
 #include <iostream>
 #include <fstream>
-#include "AddSub.h"
 #include "Design.h"
 #include "Multiplier.h"
+#include "cxxopts.hpp"
 
 using namespace std;
 
-int main() {
-    //Slicel test_slicel = Slicel("TEST");
+int main(int argc, char* argv[]) {
+    cxxopts::Options options("XDLCoreGen", "Creates a XDL description of a multiplier.");
+
+    int a_size = 8;
+    int b_size = 8;
+    bool is_pipelined = false;
+
+
+    options.add_options()
+            ("a,a_length", "A input length. Default: 8", cxxopts::value<int>(), "N")
+            ("b,b_length", "A input length. Default: 8", cxxopts::value<int>(), "N")
+            ("o,output", "Output file path. If omitted the output will be printed.", cxxopts::value<std::string>(), "<path>")
+            ("p,pipelined", "Generate pipelined Multiplier. Default: not pipelined")
+            ("h,help", "Print help")
+            ;
+
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help"))
+    {
+        std::cout << options.help({""}) << std::endl;
+        return 0;
+    }
+
+    if(result.count("a")) {
+        a_size = result["a"].as<int>();
+    }
+
+    if(result.count("b")) {
+        b_size = result["b"].as<int>();
+    }
+
+    is_pipelined = (result.count("p") != 0);
+
     Device d = Device("xc6vlx75tff484-3", "./devices/xc6vlx75tff484-3.xdl");
-    //cout<<d;
-    //AddSub test_addsub = AddSub(32);
-    Multiplier test_multi = Multiplier(8, 8, true);
+    Multiplier test_multi = Multiplier(a_size, b_size, is_pipelined);
     Design test_design = Design("__XILINX_NMC_MACRO", d);
     test_design.add_module(test_multi);
     test_design.place();
 
 
-    //cout << test_slicel.to_string() << endl;
-    //cout << "\n AddSub:\n" << endl << test_addsub.to_string();
-    //cout << test_design;
-
-
-    ofstream outfile;
-    outfile.open("C:\\Users\\prozac\\xdl\\test.xdl");
-    outfile << test_design;
-    outfile.close();
-
+    if(result.count("output")) {
+        ofstream outfile;
+        outfile.open(result["output"].as<std::string>());
+        outfile << test_design;
+        outfile.close();
+    }
+    else {
+        cout << test_design;
+    }
 
 
     return 0;
